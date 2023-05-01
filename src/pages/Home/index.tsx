4058b9c97a6550147dbf3e713d4c34e65b4b664e
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-05-02 00:06:15 +0300

    finish the challenge

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,14 +26,15 @@ const Home = (): JSX.Element => {
   const { addProduct, cart } = useCart();
 
   const cartItemsAmount = cart.reduce((sumAmount, product) => {
-    // TODO
-    sumAmount.id += product.amount
+    const newSumAmount = {...sumAmount}  // DONE
+    newSumAmount[product.id] = product.amount
 
+    return newSumAmount;
   }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      // DONE
       await api.get('/products')
       .then(response => setProducts(response.data))
     }
@@ -42,25 +43,25 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id)  // DONE
   }
 
   return (
     <ProductList>
       {
         products.map(product => (
-          <li>
+          <li key={product.id}>
             <img src={product.image} alt={product.title} />
             <strong>Tênis de Caminhada Leve Confortável</strong>
             <span>{ formatPrice(product.price) }</span>
             <button
               type="button"
               data-testid="add-product-button"
-            // onClick={() => handleAddProduct(product.id)}
+            onClick={() => handleAddProduct(product.id)}
             >
               <div data-testid="cart-product-quantity">
                 <MdAddShoppingCart size={16} color="#FFF" />
-                {/* {cartItemsAmount[product.id] || 0} */} 2
+                {cartItemsAmount[product.id] || 0}
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
